f6b8f1a542c1b8cbfe4c5ca19cc80b7cc14035c6
cs6axl-bit <cs6axl@gmail.com>
2026-03-14 20:37:57 +0200

    Update user_notifications_extension.rb

--- lib/digest_campaigns/user_notifications_extension.rb
+++ lib/digest_campaigns/user_notifications_extension.rb
@@ -1430,7 +1430,7 @@ unsub_link_text = "Unsubscribe" if unsub_link_text.blank?
 
 unsub_html = <<~HTML
   <div style="font-size:12px; color:#000; line-height:1.4; padding: 10px 0; text-align:center;">
-    #{ERB::Util.html_escape(unsub_text)} <a href="#{ERB::Util.html_escape(unsub_url)}" style="color:#000; text-decoration:underline;">#{ERB::Util.html_escape(unsub_link_text)}</a>
+    #{ERB::Util.html_escape(unsub_text)} <a href="#{ERB::Util.html_escape(unsub_url)}" style="color:#000; text-decoration:underline; font-weight:normal;">#{ERB::Util.html_escape(unsub_link_text)}</a>
   </div>
 HTML
 
